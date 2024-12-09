3004341629910ef329f212f145962bc5fc606e88
Sarah D. <mysagesierra@gmail.com>
2024-12-09 14:43:08 +0800

    UI landing page

--- CentralComm.js
+++ CentralComm.js
@@ -11,10 +11,23 @@ document.addEventListener("DOMContentLoaded", function () {
     }, 3000); // Display duration of splash screen
 
     $(window).on('scroll load', function () {
+
         if ($(window).scrollTop() > 30) {
             $('header').addClass('header-active');
         } else {
             $('header').removeClass('header-active');
         }
+
+        $('section').each(function () {
+            var offset = $(window).scrollTop();
+            var id = $(this).attr('id');
+            var top = $(this).offset().top - 200;
+            var height = $(this).height();
+
+            if (offset >= top && offset < top + height) {
+                $('.navbar ul li a').removeClass('active');
+                $('.navbar').find(`[href="#${id}"]`).addClass('active');
+            }
+        });
     });
 });
